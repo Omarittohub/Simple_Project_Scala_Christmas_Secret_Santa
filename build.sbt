61f66413419f22fbaa3ef0484424cb42f9669c85
Omarittohub <57007157+Omarittohub@users.noreply.github.com>
2025-12-13 19:01:17 +0100

    V1 Project

---
+++ build.sbt
@@ -0,0 +1,14 @@
+val scala3Version = "3.3.4"
+
+lazy val root = project
+  .in(file("."))
+  .settings(
+    name := "whisper-jar",
+    version := "0.1.0-SNAPSHOT",
+    scalaVersion := scala3Version,
+    libraryDependencies ++= Seq(
+      "dev.zio" %% "zio"      % "2.1.1",
+      "dev.zio" %% "zio-http" % "3.0.0-RC6",
+      "dev.zio" %% "zio-json" % "0.6.2"
+    )
+  )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,119 @@
+import zio._
+import zio.http._
+import zio.http.ChannelEvent.Read
+import zio.http.Middleware
+import zio.json._
+import zio.stream._
+import java.time.Instant
+
+//Les modeles de donnees
+case class CreateWhisper(content: String)
+object CreateWhisper {
+  implicit val codec: JsonCodec[CreateWhisper] = DeriveJsonCodec.gen
+}
+
+case class Whisper(content: String, timestamp: Long)
+object Whisper {
+  implicit val codec: JsonCodec[Whisper] = DeriveJsonCodec.gen
+}
+
+//L'appli principale
+object Main extends ZIOAppDefault {
+
+  def run =
+    for {
+      db  <- Ref.make(Vector(Whisper("Seconde Chance", Instant.now.getEpochSecond)))
+      hub <- Hub.unbounded[Whisper]
+      app = (routes(db, hub) @@ Middleware.cors) 
+      .handleError(e => Response.internalServerError(s"Erreur: ${e.getMessage}") ) 
+      .toHttpApp
+
+      _ <- Server.serve(app).provide(Server.default)
+    } yield ()
+
+
+  def routes(db: Ref[Vector[Whisper]], hub: Hub[Whisper]) = Routes(
+
+    // ping-pong (check que le serveur tourne)
+    Method.GET / "ping" -> handler(Response.text("pong")),
+
+    // obtenir un secret aleatoire de la bdd
+    Method.GET / "random" -> handler {
+      for {
+        items <- db.get
+        secret <- Random.nextIntBounded(items.size).map(items(_))
+      } yield Response.json(secret.toJson)
+    },
+
+    // creer un nouveau secret et le mettre dans la bdd
+    Method.POST / "whisper" -> handler { (req: Request) =>
+      req.body.asString.flatMap { body =>
+        body.fromJson[CreateWhisper] match {
+          case Left(err) => ZIO.succeed(Response.badRequest(err))
+          case Right(input) =>
+            val newWhisper = Whisper(input.content, Instant.now.getEpochSecond)
+            db.update(_ :+ newWhisper) *>
+            hub.publish(newWhisper) *>
+            ZIO.succeed(Response.ok)
+        }
+      }
+    },
+
+    // Lire les noms
+    Method.GET / "whispers" -> handler {
+      db.get.map(ws => Response.json(ws.toJson))
+    },
+
+    // lire un nom par index
+    Method.GET / "whisper" / int("index") -> handler { (index: Int, _: Request) =>
+      db.get.map { ws =>
+        if (index < 0 || index >= ws.size)
+          Response.status(Status.NotFound)
+        else
+          Response.json(ws(index).toJson)
+      }
+    },
+
+    // changer un nom par index
+    Method.PUT / "whisper" / int("index") -> handler { (index: Int, req: Request) =>
+      req.body.asString.flatMap { body =>
+        body.fromJson[CreateWhisper] match {
+          case Left(err) =>
+            ZIO.succeed(Response.badRequest(err))
+          case Right(input) =>
+            db.modify { ws =>
+              if (index < 0 || index >= ws.size)
+                (Response.status(Status.NotFound), ws)
+              else {
+                val updated = ws.updated(index, ws(index).copy(content = input.content))
+                (Response.text("Nom updated"), updated)
+              }
+            }
+        }
+      }
+    } ,
+
+    // enlever un nom par index
+    Method.DELETE / "whisper" / int("index") -> handler { (index: Int, _: Request) =>
+      db.modify { ws =>
+        if (index < 0 || index >= ws.size)
+          (Response.status(Status.NotFound), ws)
+        else
+          (Response.text("Nom deleted"), ws.patch(index, Nil, 1))
+      }
+    },
+
+
+
+    // la WBS pour strean les messages
+    Method.GET / "stream" -> handler {
+      val socket = Handler.webSocket { channel =>
+        ZStream.fromHub(hub)
+          .map(w => WebSocketFrame.text(w.toJson))
+          .map(frame => Read(frame))
+          .foreach(channel.send)
+      }
+      socket.toResponse
+    }
+  )
+}
